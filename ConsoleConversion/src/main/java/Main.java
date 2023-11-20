d5352f7b98227b05892714cff307c47fffa284f1
princej24 <116045993+princej24@users.noreply.github.com>
2023-11-19 22:01:35 -0800

    done

--- ConsoleConversion/src/main/java/Main.java
+++ ConsoleConversion/src/main/java/Main.java
@@ -5,8 +5,7 @@ public class Main {
         Players player = new Players("John", 42);
         System.out.println("Name: " + player.getName());
         System.out.println("Number: " + player.getNumber());
-
-        // You can also update the values using the setter methods
+        
         player.setName("Alice");
         player.setNumber(17);
         System.out.println("Updated Name: " + player.getName());
---
+++ ConsoleConversion/src/main/java/Players.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jeffm
+ */
+public class Players {
+    
+
+    private String name;
+    private int number;
+
+    
+    public Players(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
+
